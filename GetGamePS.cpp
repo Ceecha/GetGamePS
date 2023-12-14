5ca7c1b2a18f6e7c4336b6fa08847f384b95e0f3
AlfarisSwara <152954513+AlfarisSwara@users.noreply.github.com>
2023-12-14 09:59:08 +0700

    Update GetGamePS.cpp
    alfaris mengubah program

--- GetGamePS.cpp
+++ GetGamePS.cpp
@@ -1,191 +1,302 @@
 #include <iostream>
-#include <string>
 #include <algorithm>
-
 using namespace std;
 
-// Struct untuk menyimpan informasi pemain
-struct Pemain {
-    string nama;
-    int id;
+// Membuat struct untuk menyimpan data penyewa
+struct Penyewa {
+    string nama; // Nama penyewa
+    int id; // ID penyewa
+    int versi; // Versi PS yang disewa
+    bool stick; // Apakah menyewa stick PS atau tidak
+    bool tv; // Apakah menyewa TV atau tidak
+    Penyewa* next; // Pointer ke penyewa selanjutnya
 };
 
-// Struct untuk menyimpan informasi game
-struct Game {
-    string nm_game;
-    int id;
-};
-
-int main() {
-    const int MAX_PEMAIN = 10;
-    const int MAX_GAME = 5;
-
-    Pemain pemain[MAX_PEMAIN];
-    Game game[MAX_GAME];
-
-    int jmlhPemain = 0;
-    int jmlhGame = 0;
-
-    int* playerQueue = new int[MAX_PEMAIN]; // menggunakan array sebagai antrian pemain
-    int queueFront = 0;
-    int queueRear = 0;
-
-    while (true) {
-        cout << "=== Rental PS Management ===" << endl;
-        cout << "1. Tambah Pemain" << endl;
-        cout << "2. Keluarkan Pemain" << endl;
-        cout << "3. Lihat Antrian Pemain" << endl;
-        cout << "4. Tambah Game" << endl;
-        cout << "5. Pilih Game" << endl;
-        cout << "6. Lihat Status PS" << endl;
-        cout << "7. Keluar dari Program" << endl;
-
-        int pilih;
-        cout << "Pilih menu (1-8): ";
-        cin >> pilih;
-
-        switch (pilih) {
-            case 1: // Tambah Pemain
-                if (jmlhPemain < MAX_PEMAIN) {
-                    Pemain pemainBaru;
-                    cout << "Masukkan nama pemain: ";
-                    cin >> pemainBaru.nama;
-                    pemainBaru.id = jmlhPemain + 1;
-                    pemain[jmlhPemain++] = pemainBaru;
-                    playerQueue[queueRear] = jmlhPemain - 1;
-                    queueRear = (queueRear + 1) % MAX_PEMAIN;
-                    cout << "Pemain berhasil ditambahkan." << endl;
-                } else {
-                    cout << "Antrian pemain penuh. Tidak dapat menambahkan pemain baru." << endl;
+// Membuat class untuk menyimpan data penyewa menggunakan array of linked lists
+class RentalPS {
+    private:
+        int ukuran; // Ukuran array
+        Penyewa** arr; // Array of pointers ke linked lists
+    public:
+        // Constructor RentalPS untuk inisialisasi array dengan ukuran 's'
+        RentalPS (int s) {
+            ukuran = s;
+            arr = new Penyewa* [ukuran];
+            for (int i = 0; i < ukuran; i++) {
+                arr [i] = NULL;
+            }
+        }
+        
+        // Destructor RentalPS untuk menghapus setiap elemen linked list dan array
+        ~RentalPS () {
+            for (int i = 0; i < ukuran; i++) {
+                Penyewa* temp = arr [i];
+                while (temp != NULL) {
+                    Penyewa* next = temp->next;
+                    delete temp;
+                    temp = next;
                 }
-                cout << endl;
-                break;
+            }
+            delete [] arr;
+        }
 
-            case 2: // Keluarkan Pemain
-                if (queueFront != queueRear) {
-                    int removedPlayerIndex = playerQueue[queueFront];
-                    cout << pemain[removedPlayerIndex].nama << " keluar dari antrian." << endl;
-                    queueFront = (queueFront + 1) % MAX_PEMAIN;
-                } else {
-                    cout << "Antrian pemain kosong. Tidak ada pemain untuk dikeluarkan." << endl;
+        // Fungsi untuk mengembalikan indeks array berdasarkan id penyewa
+        int hash (int id) {
+            return id % ukuran;
+        }
+
+        // Fungsi untuk menambahkan data penyewa ke array of linked lists
+        void tambah (string nama, int id, int versi, bool stick, bool tv) {
+            Penyewa* baru = new Penyewa;
+            baru->nama = nama;
+            baru->id = id;
+            baru->versi = versi;
+            baru->stick = stick;
+            baru->tv = tv;
+            baru->next = NULL;
+            int indeks = hash (id);
+            if (arr [indeks] == NULL) {
+                arr [indeks] = baru;
+            } else {
+                Penyewa* temp = arr [indeks];
+                while (temp->next != NULL) {
+                    temp = temp->next;
                 }
-                cout << endl;
-                break;
+                temp->next = baru;
+            }
+        }
 
-            case 3: // Lihat Antrian Pemain
-                if (queueFront != queueRear) {
-                    cout << "Antrian Pemain:" << endl;
-                    int i = queueFront;
-                    do {
-                        cout << pemain[playerQueue[i]].id << ". " << pemain[playerQueue[i]].nama << endl;
-                        i = (i + 1) % MAX_PEMAIN;
-                    } while (i != queueRear);
-                } else {
-                    cout << "Antrian pemain kosong." << endl;
-                }
-                cout << endl;
-                break;
+        // Fungsi untuk menghapus data penyewa dari array of linked list
+        void hapus (int id) {
+            int indeks = hash (id);
+            Penyewa* temp = arr [indeks];
+            Penyewa* prev = NULL;
+            while (temp != NULL && temp->id != id) {
+                prev = temp;
+                temp = temp->next;
+            }
+            if (temp == NULL) {
+                cout << "Data tidak ditemukan\n";
+                return;
+            }
+            if (prev == NULL) {
+                arr [indeks] = temp->next;
+            } else {
+                prev->next = temp->next;
+            }
+            delete temp;
+        }
 
-            case 4: // Tambah Game
-                if (jmlhGame < MAX_GAME) {
-                    Game gameBaru;
-                    cout << "Masukkan nama game: ";
-                    cin >> gameBaru.nm_game;
-                    gameBaru.id = jmlhGame + 1;
-                    game[jmlhGame++] = gameBaru;
-                    cout << "Game berhasil ditambahkan." << endl;
-                } else {
-                    cout << "Daftar game penuh. Tidak dapat menambahkan game baru." << endl;
-                }
-                cout << endl;
-                break;
+        // Fungsi untuk mencari data penyewa berdasarkan id
+        Penyewa* cari (int id) {
+            int indeks = hash (id);
+            Penyewa* temp = arr [indeks];
+            while (temp != NULL && temp->id != id) {
+                temp = temp->next;
+            }
+            return temp;
+        }
+        
+// Fungsi ini sebagai comparator untuk pengurutan berdasarkan nama
+static bool comparePenyewa(Penyewa* a, Penyewa* b) {
+    return a->nama < b->nama;
+}
 
-            case 5: // Pilih Game
-			    if (jmlhGame > 0) {
-			        cout << "Daftar Game:" << endl;
-			        for (int i = 0; i < jmlhGame; ++i) {
-			            cout << game[i].id << ". " << game[i].nm_game << endl;
-			        }
-			
-			        int idGame;
-			        cout << "Masukkan ID game yang dipilih: ";
-			        cin >> idGame;
-			
-			        if (idGame > 0 && idGame <= jmlhGame) {
-			            cout << "Anda memilih game: " << game[idGame - 1].nm_game << endl;
-			        } else {
-			            cout << "ID game tidak valid." << endl;
-			        }
-			    } else {
-			        cout << "Daftar game kosong." << endl;
-			    }
-			    cout << endl;
-			    break;
-
-            case 6: // Lihat Status PS
-                // Implementasikan sesuai kebutuhan
-                cout << "Status PS: Normal" << endl;
-                cout << endl;
-                break;
-
-            case 7: // Keluar dari Program
-                cout << "<Terima kasih telah bermain di Rental PS kami (SYALALA PS Group)>" << endl;
-                delete[] playerQueue; // hapus alokasi memori dinamis
-                return 0;
-
-            default:
-                cout << "Pilihan tidak valid. Silakan pilih kembali." << endl;
+// mengurutkan data penyewa berdasarkan nama.
+void urutkan() {
+    // Membuat array of pointers ke penyewa
+    Penyewa** data = new Penyewa*[ukuran];
+    int n = 0; // Jumlah data yang ada
+    for (int i = 0; i < ukuran; i++) {
+        Penyewa* temp = arr[i];
+        while (temp != NULL) {
+            data[n] = temp;
+            n++;
+            temp = temp->next;
         }
     }
+    // Menggunakan sort dengan custom comparator function
+    sort(data, data + n, comparePenyewa);
+    // Menampilkan data yang sudah diurutkan
+    cout << "Data penyewa yang sudah diurutkan berdasarkan nama:\n";
+    for (int i = 0; i < n; i++) {
+        cout << "Nama : " << data[i]->nama << endl << "ID :" << data[i]->id << endl << "PS : " << data[i]->versi << endl << "Sewa Stick : ";
+                if (data[i]->stick) cout << "Ya ";
+                else cout << "Tidak "; cout << endl;
+                cout << "Sewa TV : ";
+                if (data[i]->tv) cout << "Ya\n";
+                else cout << "Tidak\n";
+                cout << endl;
+    }
+    delete[] data;
+}
 
+
+        // Fungsi untuk menampilkan data penyewa yang sedang menggunakan PS, stick, dan TV
+        void tampilkan () {
+            // Membuat stack untuk menyimpan data penyewa yang menggunakan PS
+            Penyewa** stack = new Penyewa* [ukuran];
+            int top = -1; // Indeks top dari stack
+            // Membuat queue untuk menyimpan data penyewa yang menggunakan stick
+            Penyewa** queue = new Penyewa* [ukuran];
+            int front = 0; // Indeks front dari queue
+            int rear = -1; // Indeks rear dari queue
+            // Menelusuri array of linked lists
+            for (int i = 0; i < ukuran; i++) {
+                Penyewa* temp = arr [i];
+                while (temp != NULL) {
+                    // Jika menggunakan PS, masukkan ke stack
+                    if (temp->versi > 0) {
+                        top++;
+                        stack [top] = temp;
+                    }
+                    // Jika menggunakan stick, masukkan ke queue
+                    if (temp->stick) {
+                        rear++;
+                        queue [rear] = temp;
+                    }
+                    temp = temp->next;
+                }
+            }
+            // Menampilkan data penyewa yang menggunakan PS dari stack
+            cout << "Data penyewa yang menggunakan PS:\n";
+            while (top >= 0) {
+                Penyewa* p = stack [top];
+                cout << "Nama : " << p->nama << endl << "ID :" << p->id << endl << "PS : " << p->versi << endl << "Sewa Stick : ";
+                if (p->stick) cout << "Ya ";
+                else cout << "Tidak "; cout << endl;
+                cout << "Sewa TV : ";
+                if (p->tv) cout << "Ya\n";
+                else cout << "Tidak\n";
+                top--;
+                cout << endl;
+            }
+            // Menampilkan data penyewa yang menggunakan stick dari queue
+            cout << "Data penyewa yang menggunakan stick:\n";
+            while (front <= rear) {
+                Penyewa* p = queue [front];
+                cout << "Nama : " << p->nama << endl << "ID :" << p->id << endl << "PS : " << p->versi << endl << "Sewa Stick : ";
+                if (p->stick) cout << "Ya ";
+                else cout << "Tidak "; cout << endl;
+                cout << "Sewa TV : ";
+                if (p->tv) cout << "Ya\n";
+                else cout << "Tidak\n";
+                front++;
+                cout << endl;
+            }
+            delete [] stack;
+            delete [] queue;
+        }
+};
+
+// Fungsi untuk menampilkan menu pilihan kepada pengguna
+void menu () {
+    cout << "Menu pilihan:\n";
+    cout << "1. Tambah data penyewa\n";
+    cout << "2. Hapus data penyewa\n";
+    cout << "3. Cari data penyewa\n";
+    cout << "4. Urutkan data penyewa\n";
+    cout << "5. Tampilkan data penyewa\n";
+    cout << "6. Keluar\n";
+    cout << "Masukkan pilihan Anda: ";
+}
+
+// Fungsi utama
+int main () {
+    // Membuat objek RentalPS dengan ukuran array 10
+    RentalPS rps (10);
+    // Membuat variabel untuk menyimpan pilihan pengguna
+    int pilihan;
+    // Membuat loop untuk menjalankan program
+    do {
+        // Menampilkan menu pilihan
+        menu ();
+        // Membaca pilihan pengguna
+        cin >> pilihan;
+        cout << endl;
+        // Menjalankan program sesuai dengan pilihan
+        switch (pilihan) {
+            case 1: // Tambah data penyewa
+                {
+                    // Membuat variabel untuk menyimpan data penyewa
+                    string nama;
+                    int id, versi;
+                    bool stick, tv;
+                    char s, t;
+                    // Membaca data penyewa dari pengguna
+                    cout << "Masukkan nama penyewa: ";
+                    cin >> nama;
+                    cout << "Masukkan id penyewa: ";
+                    cin >> id;
+                    cout << "Masukkan versi PS yang disewa (1,2, atau 3): ";
+                    cin >> versi;
+                    cout << "Apakah menyewa stick PS? (Y/N): ";
+                    cin >> s;
+                    stick = (s == 'Y' || s == 'y') ? true : false;
+                    cout << "Apakah menyewa TV? (Y/N): ";
+                    cin >> t;
+                    tv = (t == 'Y' || t == 'y') ? true : false;
+                    // Menambah data penyewa ke RentalPS
+                    rps.tambah(nama, id, versi, stick, tv);
+                    cout << "Data penyewa berhasil ditambahkan.\n";
+                    cout << endl;
+                    break;
+                }
+            case 2: // Hapus data penyewa
+                {
+                    // Membuat variabel untuk menyimpan id penyewa yang akan dihapus
+                    int id;
+                    // Membaca id penyewa dari pengguna
+                    cout << "Masukkan id penyewa yang akan dihapus: ";
+                    cin >> id;
+                    // Menghapus data penyewa dari RentalPS
+                    rps.hapus(id);
+                    cout << endl;
+                    break;
+                }
+            case 3: // Cari data penyewa
+                {
+                    // Membuat variabel untuk menyimpan id penyewa yang akan dicari
+                    int id;
+                    // Membaca id penyewa dari pengguna
+                    cout << "Masukkan id penyewa yang akan dicari: ";
+                    cin >> id;
+                    // Mencari data penyewa di RentalPS
+                    Penyewa* hasil = rps.cari(id);
+                    // Menampilkan hasil pencarian
+                    if (hasil != NULL) {
+                        cout << "Data ditemukan:\n";
+                        cout << "Nama : " << hasil->nama << endl;
+                        cout << "ID : " << hasil->id << endl;
+                        cout << "PS : " << hasil->versi << endl;
+                        cout << "Sewa Stick : " << (hasil->stick ? "Ya" : "Tidak") << endl;
+                        cout << "Sewa TV : " << (hasil->tv ? "Ya" : "Tidak") << endl;
+                    } else {
+                        cout << "Data tidak ditemukan.\n";
+                    }
+                    cout << endl;
+                    break;
+                }
+            case 4: // Urutkan data penyewa
+                rps.urutkan();
+                cout << endl;
+                break;
+            case 5: // Tampilkan data penyewa
+                rps.tampilkan();
+                cout << endl;
+                break;
+            case 6: // Keluar
+                cout << "Program selesai. Terima kasih!\n";
+                cout << endl;
+                break;
+            default:
+                cout << "Pilihan tidak valid. Silakan coba lagi.\n";
+                cout << endl;
+                break;
+        }
+    } while (pilihan != 6);
     return 0;
 }
 
-/* Program C++ di atas adalah implementasi sederhana untuk manajemen rental PS dengan beberapa fitur seperti menambahkan pemain, 
-mengeluarkan pemain, melihat antrian pemain, menambah game, memilih game, dan melihat status PS. 
-Berikut adalah penjelasan singkat untuk setiap bagian program:
-
-1. Struct Pemain dan Game:
-	* Dua struktur, 'Pemain' dan 'Game', digunakan untuk menyimpan informasi pemain dan game.
-	
-2. Deklarasi Variabel dan Konstanta:
-	* MAX_PEMAIN dan MAX_GAME adalah konstanta yang menentukan batas maksimum pemain dan game.
-	* 'pemain' dan 'game' adalah array untuk menyimpan data pemain dan game.
-	* 'jmlhPemain' dan 'jmlhGame' adalah variabel untuk menghitung jumlah pemain dan game.
-	* 'playerQueue' adalah array dinamis sebagai antrian pemain dengan konsep circular queue.
-	
-3. Loop Utama:
-	* Program berjalan dalam loop utama yang terus berjalan hingga pengguna memilih keluar.
-
-4. Menu Utama:
-	* Menampilkan opsi-opsi menu kepada pengguna.
-
-5. Switch Case untuk Opsi Menu:
-	* Program menggunakan 'switch' untuk menangani berbagai opsi menu yang dapat dipilih oleh pengguna.
-
-6. Tambah Pemain (Case 1):
-	* Meminta pengguna untuk memasukkan nama pemain baru.
-	* Menambahkan pemain ke dalam array 'pemain' dan ID pemain ke dalam antrian 'playerQueue'.
-
-7. Keluarkan Pemain (Case 2):
-	* Mengeluarkan pemain dari antrian setelah selesai bermain dan menampilkan nama pemain yang keluar.
-
-8. Lihat Antrian Pemain (Case 3):
-	* Menampilkan antrian pemain menggunakan circular queue.
-
-9. Tambah Game (Case 4):
-	* Meminta pengguna untuk memasukkan nama game baru.
-	* Menambahkan game ke dalam array game.
-
-10. Pilih Game (Case 5):
-	* Menampilkan daftar game dan meminta pengguna untuk memilih game berdasarkan ID.
-
-11. Lihat Status PS (Case 6):
-	* Menampilkan status PS, dalam hal ini ditampilkan "Normal".
-
-12. Keluar dari Program (Case 7):
-	* Memberikan pesan terima kasih dan menghapus alokasi memori dinamis untuk playerQueue.
-
-*/
 
 
